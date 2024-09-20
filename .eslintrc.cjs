63725ec4e9b3a474ee291e2a7f4036697060f2b7
YusufHabib317 <123254927+YusufHabib317@users.noreply.github.com>
2024-09-20 17:59:27 +0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,29 @@
 module.exports = {
-  extends: ['plugin:@next/next/recommended', 'plugin:jest/recommended'],
-  plugins: ['testing-library', 'jest'],
+  extends: [
+    'plugin:@next/next/recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended', 
+  ],
+  plugins: ['testing-library', 'jest', '@typescript-eslint'], 
   overrides: [
     {
       files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
       extends: ['plugin:testing-library/react'],
     },
   ],
+  parser: '@typescript-eslint/parser', 
   parserOptions: {
     project: './tsconfig.json',
+    ecmaVersion: 2020, 
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, 
+    },
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    'react/react-in-jsx-scope': 'off', 
     'import/extensions': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
